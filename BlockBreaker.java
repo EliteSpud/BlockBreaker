61d2707a6118a6fd15a615ecd53f20c292a743a8
EliteSpud <37165449+EliteSpud@users.noreply.github.com>
2018-04-24 13:06:02 +0100

    Commit 2
    Added gameStyle.css to allow me to style the window with css code.
    Currently just makes the window a dark blue colour.
    Editied the BlockBreaker.java file to open the css file and use it to style the scene.

--- BlockBreaker.java
+++ BlockBreaker.java
@@ -19,7 +19,9 @@ public class BlockBreaker extends Application
 		
 		
 		StackPane rootPane = new StackPane();
-		mainStage.setScene(new Scene(rootPane,504,896)); //9:16 aspect ratio, divisible by 8
+		Scene scene = new Scene(rootPane,504,896); //9:16 aspect ratio, divisible by 8
+		mainStage.setScene(scene); 
+		scene.getStylesheets().add(BlockBreaker.class.getResource("gameStyle.css").toExternalForm());
 		mainStage.show();
 	}
 	
