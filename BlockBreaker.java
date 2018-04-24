d0d8ee79e8535189d6fc1aa43fc66321de01f82c
EliteSpud <37165449+EliteSpud@users.noreply.github.com>
2018-04-24 21:32:36 +0100

    Commit 3
    Added a Fire button that listens for when it is clicked. In future this will be used to command the balls to be fired at the blocks, once the user has chosen a direction.

--- BlockBreaker.java
+++ BlockBreaker.java
@@ -1,8 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Paint;
+import java.awt.Color;
+import javafx.scene.text.*;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 
 public class BlockBreaker extends Application
@@ -18,10 +24,41 @@ public class BlockBreaker extends Application
 		
 		
 		
-		StackPane rootPane = new StackPane();
-		Scene scene = new Scene(rootPane,504,896); //9:16 aspect ratio, divisible by 8
-		mainStage.setScene(scene); 
+		Pane root = new Pane(); //root of the scene
+		Scene scene = new Scene(root,504,896); //9:16 aspect ratio, divisible by 8
+		Pane gamePane = new Pane();
+		Pane rightPane = new Pane();
+		
+		Rectangle scoreRect = new Rectangle(112,63,Paint.valueOf("0000FF"));
+		scoreRect.relocate(392,0);
+		
+		StackPane stackFire = new StackPane();
+		Rectangle fireRect = new Rectangle(112,190,Paint.valueOf("FF0000"));
+		fireRect.setOnMousePressed(new EventHandler<MouseEvent>(){
+			public void handle(MouseEvent event)
+			{
+				System.out.println("FIRE CLICKED");
+				//fire
+			}
+		});
+		//fireRect.relocate(392,706);
+		Text fireText = new Text("FIRE");
+		fireText.setOnMousePressed(new EventHandler<MouseEvent>(){
+			public void handle(MouseEvent event)
+			{
+				System.out.println("FIRE CLICKED");
+				//fire
+			}
+		});
+		stackFire.getChildren().addAll(fireRect,fireText);
+		stackFire.setLayoutX(392);
+		stackFire.setLayoutY(706);
+		
+		rightPane.getChildren().addAll(scoreRect,stackFire);
+		
+		root.getChildren().addAll(gamePane,rightPane);
 		scene.getStylesheets().add(BlockBreaker.class.getResource("gameStyle.css").toExternalForm());
+		mainStage.setScene(scene); 
 		mainStage.show();
 	}
 	
